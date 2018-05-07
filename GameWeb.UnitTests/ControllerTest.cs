15e816d288ba7eed7f9b6d9fb11d113f13752e2f
StardustZiggy <khardlif1@gmail.com>
2018-05-07 21:28:03 +0200

    Minor frontend adjustments

---
+++ GameWeb.UnitTests/ControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GameWeb.UnitTests
+{
+    [TestFixture]
+    public class ControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
