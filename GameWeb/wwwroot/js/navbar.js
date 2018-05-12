b571c9e7951f7792a9412f74d703e731dc2c19b1
StardustZiggy <khardlif1@gmail.com>
2018-05-12 20:38:27 +0200

    Frontend details: Transparent navbar, blurred background photo, changed fonts

---
+++ GameWeb/wwwroot/js/navbar.js
@@ -0,0 +1,7 @@
+﻿$(window).scroll( () => {
+    if ($(this).scrollTop() > 50){
+        $("#navbar-top").removeClass("navbar-transparent");
+    } else {
+        $("#navbar-top").addClass("navbar-transparent");
+    }
+});
